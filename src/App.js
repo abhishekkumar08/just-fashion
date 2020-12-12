0369dc0dd860f54493b6f35db357608533204fb3
Abhishek Kumar <abhishek022kk@gmail.com>
2020-12-12 23:45:05 +0530

    Added header component and links to sign, contact

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import HomePage from './pages/homepage/homepage';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
