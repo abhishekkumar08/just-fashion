554f68b8b673ef5c6927b9d591f76e804d9ab2b0
Abhishek Kumar <abhishek022kk@gmail.com>
2020-12-12 14:48:48 +0530

    Updated menu-item with withRouter and added navigation

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -51,8 +51,8 @@ class Directory extends Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({ title, id, imageUrl, size }) => (
-                        <MenuItem id={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem id={id} {...otherSectionProps} />
                     ))}
             </div>
         );
